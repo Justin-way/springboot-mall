da5c242c5b5469650df49d5d2b686efd6d8f277d
Wayne Yeh <thebest77771023@yahoo.com.tw>
2023-04-16 02:02:54 +0800

    add view part

--- springboot-mall/src/main/java/com/wayneyeh/springbootmall/controller/PageController.java
+++ springboot-mall/src/main/java/com/wayneyeh/springbootmall/controller/PageController.java
@@ -2,18 +2,23 @@ package com.wayneyeh.springbootmall.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String gethome(){
         return "index";
     }
-
-    @GetMapping("/test")
-    public String getTest(){
-        return "test";
+    @GetMapping("/query_order")
+    public String queryOrder(){
+        return "query_order";
     }
+
+    @GetMapping("/create_order")
+    public String createOrder(){
+        return "create_order";
+    }
+
 }
